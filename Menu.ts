842e1ccd6e1683fef4ecfd97eab07178afb053dc
jorgeguedess <jorge_guedes00@hotmail.com>
2024-03-26 11:29:52 -0300

    feat: creation of the Account class

--- Menu.ts
+++ Menu.ts
@@ -1,117 +1,150 @@
 ﻿import readlinesync = require("readline-sync");
 import { colors } from "./src/util/Cores";
+import { Conta } from "./src/model/Conta";
 
 export function main() {
+  let opcao: number;
 
-    let opcao: number;
+  let c1: Conta = new Conta(1, 123, 1, "João Pedro", 100000);
+  c1.visualizar();
 
-    while (true) {
+  // // Modificando o meu Saldo
+  // c1.set_saldo(1500000);
 
-        console.log(colors.bg.black, colors.fg.yellow, 
-                    "*****************************************************");
-        console.log("                                                     ");
-        console.log("                BANCO DO BRAZIL COM Z                ");
-        console.log("                                                     ");
-        console.log("*****************************************************");
-        console.log("                                                     ");
-        console.log("            1 - Criar Conta                          ");
-        console.log("            2 - Listar todas as Contas               ");
-        console.log("            3 - Buscar Conta por Numero              ");
-        console.log("            4 - Atualizar Dados da Conta             ");
-        console.log("            5 - Apagar Conta                         ");
-        console.log("            6 - Sacar                                ");
-        console.log("            7 - Depositar                            ");
-        console.log("            8 - Transferir valores entre Contas      ");
-        console.log("            9 - Sair                                 ");
-        console.log("                                                     ");
-        console.log("*****************************************************");
-        console.log("                                                     ", 
-        colors.reset);
+  // // Recuperando o valor do meu Saldo
+  // console.log(c1.get_saldo());
 
-        console.log("Entre com a opção desejada: ");
-        opcao = readlinesync.questionInt("");
+  console.log(c1.sacar(2000000));
 
-        if (opcao == 9) {
-            console.log(colors.fg.greenstrong, 
-                "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
-            sobre();
-            console.log(colors.reset, "");
-            process.exit(0);
-        }
+  c1.visualizar();
 
-        switch (opcao) {
-            case 1:
-                console.log(colors.fg.whitestrong, 
-                    "\n\nCriar Conta\n\n", colors.reset);
-                
-                keyPress()
-                break;
-            case 2:
-                console.log(colors.fg.whitestrong, 
-                    "\n\nListar todas as Contas\n\n", colors.reset);
+  c1.depositar(5000);
 
-                keyPress()
-                break;
-            case 3:
-                console.log(colors.fg.whitestrong, 
-                    "\n\nConsultar dados da Conta - por número\n\n", colors.reset);
+  c1.visualizar();
 
-                keyPress()
-                break;
-            case 4:
-                console.log(colors.fg.whitestrong, 
-                    "\n\nAtualizar dados da Conta\n\n", colors.reset);
+  while (true) {
+    console.log(
+      colors.bg.black,
+      colors.fg.yellow,
+      "*****************************************************"
+    );
+    console.log("                                                     ");
+    console.log("                BANCO DO BRAZIL COM Z                ");
+    console.log("                                                     ");
+    console.log("*****************************************************");
+    console.log("                                                     ");
+    console.log("            1 - Criar Conta                          ");
+    console.log("            2 - Listar todas as Contas               ");
+    console.log("            3 - Buscar Conta por Numero              ");
+    console.log("            4 - Atualizar Dados da Conta             ");
+    console.log("            5 - Apagar Conta                         ");
+    console.log("            6 - Sacar                                ");
+    console.log("            7 - Depositar                            ");
+    console.log("            8 - Transferir valores entre Contas      ");
+    console.log("            9 - Sair                                 ");
+    console.log("                                                     ");
+    console.log("*****************************************************");
+    console.log(
+      "                                                     ",
+      colors.reset
+    );
 
-                keyPress()
-                break;
-            case 5:
-                console.log(colors.fg.whitestrong, 
-                    "\n\nApagar uma Conta\n\n", colors.reset);
+    console.log("Entre com a opção desejada: ");
+    opcao = readlinesync.questionInt("");
 
-                keyPress()
-                break;
-            case 6:
-                console.log(colors.fg.whitestrong, 
-                    "\n\nSaque\n\n", colors.reset);
-
-                keyPress()
-                break;
-            case 7:
-                console.log(colors.fg.whitestrong, 
-                    "\n\nDepósito\n\n", colors.reset);
-
-                keyPress()
-                break;
-            case 8:
-                console.log(colors.fg.whitestrong, 
-                    "\n\nTransferência entre Contas\n\n", colors.reset);
-
-                keyPress()
-                break;
-            default:
-                console.log(colors.fg.whitestrong, 
-                    "\nOpção Inválida!\n", colors.reset);
-
-                keyPress()
-                break;
-        }
+    if (opcao == 9) {
+      console.log(
+        colors.fg.greenstrong,
+        "\nBanco do Brazil com Z - O seu Futuro começa aqui!"
+      );
+      sobre();
+      console.log(colors.reset, "");
+      process.exit(0);
     }
 
+    switch (opcao) {
+      case 1:
+        console.log(colors.fg.whitestrong, "\n\nCriar Conta\n\n", colors.reset);
+
+        keyPress();
+        break;
+      case 2:
+        console.log(
+          colors.fg.whitestrong,
+          "\n\nListar todas as Contas\n\n",
+          colors.reset
+        );
+
+        keyPress();
+        break;
+      case 3:
+        console.log(
+          colors.fg.whitestrong,
+          "\n\nConsultar dados da Conta - por número\n\n",
+          colors.reset
+        );
+
+        keyPress();
+        break;
+      case 4:
+        console.log(
+          colors.fg.whitestrong,
+          "\n\nAtualizar dados da Conta\n\n",
+          colors.reset
+        );
+
+        keyPress();
+        break;
+      case 5:
+        console.log(
+          colors.fg.whitestrong,
+          "\n\nApagar uma Conta\n\n",
+          colors.reset
+        );
+
+        keyPress();
+        break;
+      case 6:
+        console.log(colors.fg.whitestrong, "\n\nSaque\n\n", colors.reset);
+
+        keyPress();
+        break;
+      case 7:
+        console.log(colors.fg.whitestrong, "\n\nDepósito\n\n", colors.reset);
+
+        keyPress();
+        break;
+      case 8:
+        console.log(
+          colors.fg.whitestrong,
+          "\n\nTransferência entre Contas\n\n",
+          colors.reset
+        );
+
+        keyPress();
+        break;
+      default:
+        console.log(colors.fg.whitestrong, "\nOpção Inválida!\n", colors.reset);
+
+        keyPress();
+        break;
+    }
+  }
 }
 
 /* Função com os dados da pessoa desenvolvedora */
 function sobre(): void {
-    console.log("\n*****************************************************");
-    console.log("Projeto Desenvolvido por: ");
-    console.log("Generation Brasil - generation@generation.org");
-    console.log("github.com/conteudoGeneration");
-    console.log("*****************************************************");
+  console.log("\n*****************************************************");
+  console.log("Projeto Desenvolvido por: ");
+  console.log("Generation Brasil - generation@generation.org");
+  console.log("github.com/conteudoGeneration");
+  console.log("*****************************************************");
 }
 
 function keyPress(): void {
-    console.log(colors.reset, "");
-    console.log("\nPressione enter para continuar...");
-    readlinesync.prompt();
+  console.log(colors.reset, "");
+  console.log("\nPressione enter para continuar...");
+  readlinesync.prompt();
 }
 
 main();
